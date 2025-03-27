40770e6c3aad8d6624e127b76732cec3aafbe30b
Oscar Ko <OscarKoEmail@gmail.com>
2025-03-27 14:12:51 -0400

    Worked on static pages

--- main.js
+++
@@ -1,8 +0,0 @@
-let testBtn = document.getElementById("test-button");
-let testText = document.getElementById("test-text");
-
-testBtn.addEventListener("click", popFunction);
-
-function popFunction() {
-  testText.innerHTML = "Clicked Success";
-}
