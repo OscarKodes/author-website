895bd4066c910dfa23b4da3852ecbbbea33bd8a4
oscarkodes <oscarkoemail@gmail.com>
2026-05-25 10:43:44 -0400

    Make NavBar modular into main.js. Make footer modular. Hide footer and about page.

---
+++ main.js
@@ -0,0 +1,48 @@
+ 
+//  NAVBAR ------------------
+
+ document.getElementById("navBar").innerHTML =
+    '<li class="nav-item">'
+        +'<a class="nav-link text-dark" aria-current="page" href="index.html">Home</a>'
+    +'</li>'
+
+    +'<li class="nav-item dropdown">'
+        +'<a class="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown"'
+            +'aria-expanded="false">'
+                +'Books'
+        +'</a>'
+        +'<ul class="dropdown-menu">'
+            +'<li><a class="dropdown-item" href="book_takeout-witches.html">The Takeout Witches</a></li>'
+        +'</ul>'
+    +'</li>'
+    
+    // // About Page
+    
+    // +'<li class="nav-item">'
+    //     +'<a class="nav-link text-dark" href="about.html">About</a>'
+    // +'</li>'
+
+
+
+
+// // FOOTER ------------------
+
+// document.getElementById("footer").innerHTML =
+
+//     '<!-- CTA TEXT -->' 
+//     +'<p class="cta-text fw-semibold fs-5">' 
+//         +'SUBSCRIBE FOR UPDATES' 
+//     +'</p>' 
+
+//     +'<!-- NEWSLETTER SUBSCRIBE -->' 
+//     +'<iframe class="substack" src="https://oscarkoauthor.substack.com/embed" frameborder="0" scrolling="no">' 
+//     +'</iframe>' 
+
+//     +'<!-- SOCIAL ICONS -->' 
+//     +'<div class="icon-container">' 
+//         +'<a href="https://bsky.app/profile/oscarkoauthor.bsky.social"><img class="social-icon"' 
+//                 +'src="icons/bluesky.svg" alt=""></a>' 
+//         +'<a href="https://www.tiktok.com/@oscarkoauthor"><img class="social-icon" src="icons/tiktok.svg" alt=""></a>' 
+//         +'<a href="https://www.instagram.com/oscarkoauthor"><img class="social-icon" src="icons/instagram.svg"' 
+//                 +'alt=""></a>' 
+//     +'</div>'
